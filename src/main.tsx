d0313174b3d4d341c330b45c7fa7d48a59f01371
naved20 <naved2010m@gmail.com>
2026-08-19 19:03:04 +0530

    also for pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App.tsx";
 import "./index.css";
+
+// Automatically update and reload PWA when a new version is published
+const updateSW = registerSW({
+  onNeedRefresh() {
+    updateSW(true);
+  },
+  onOfflineReady() {
+    console.log("PWA ready for offline use");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
 
