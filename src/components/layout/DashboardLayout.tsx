d57847f4e3cbd9cdd4ce07ec8ac37efc0416668b
naved20 <naved2010m@gmail.com>
2026-09-02 20:17:53 +0530

    kjhdfh

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -84,7 +84,7 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
                       <div className="flex flex-col leading-tight">
               <h1 className="font-bold text-xl text-primary">WES OneDesk</h1>
               <span className="text-[10px] text-muted-foreground font-medium">
-                v 2.9.2.1
+                v 2.9.2.2
               </span>
             </div>
         </div>
@@ -118,7 +118,7 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
             <div className="flex flex-col leading-tight">
               <h1 className="font-bold text-xl text-primary">WES OneDesk</h1>
               <span className="text-[10px] text-muted-foreground font-medium">
-                v 2.9.2.1              </span>
+                v 2.9.2.2              </span>
             </div>
         </div>
 
